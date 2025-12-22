df92fe691d86fd4be1c1ec5c93cdcbc185ed3e51
AlexRosly <rosly.a777@gmail.com>
2025-12-23 00:47:07 +0200

    create user auth

--- old/src/repository/user.repository.js
+++
@@ -1,7 +0,0 @@
-import User from "../models/user.model.js";
-
-export const userRepository = {
-  findByGoogleId: (googleId) => User.findOne({ googleId }),
-  findById: (id) => User.findById(id),
-  create: (data) => User.create(data),
-};
--- old/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth.controller");
-const authMiddleware = require("../middlewares/auth.middleware");
-
-router.post("/google", authController.googleAuth);
-router.get("/me", authMiddleware, authController.me);
-
-module.exports = router;

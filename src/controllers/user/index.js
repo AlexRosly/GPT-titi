575b2a80113b0714ac321ae3a287ef3876a2abe0
AlexRosly <rosly.a777@gmail.com>
2025-12-28 22:31:11 +0200

    change import in file index.js user controller

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -2,7 +2,7 @@ const createUser = require("./authUser");
 const refreshToken = require("./refreshToken");
 const logout = require("./logout");
 const tokenUsage = require("./tokenUsage");
-const getUsageHistory = require("./getUsageHistory.js");
+const getUsageHistory = require("./getUsageHistory");
 
 module.exports = {
   createUser,
